361abeed5f61c55ce05552b44d84ee82623d66ab
erickfrd <erreyesde@est.utn.ac.cr>
2026-06-29 21:09:32 -0600

    Clase Persona
    Se crea la clase persona

---
+++ src/persona/Persona.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package persona;
+/**
+ *
+ * @author erick
+ */
+public class Persona {
+    protected String id;
+    protected String nombre;
+    protected String telefono;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public Persona(String id, String nombre, String telefono) {
+        this.id = id;
+        this.nombre = nombre;
+        this.telefono = telefono;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "id=" + id + ", nombre=" + nombre + ", telefono=" + telefono + '}';
+    }
+    
+    
+}
